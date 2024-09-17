828e98abb3550d31af6b0df2f5f3ccf40c88588d
Doug Lazarini <dllazarini@gmail.com>
2024-09-17 08:04:43 -0300

    Começando o site de receitas

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from recipes.views import contato, sobre, home
+from recipes.views import home
 
 
 urlpatterns = [
     path('', home),  # home
-    path('sobre/', sobre),  # sobre
-    path('contato/', contato),  # contato
 ]
