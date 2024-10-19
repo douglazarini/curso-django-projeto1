cfc1b66b8eb1f29a0534f8ec7343b1174c059626
Doug Lazarini <81652251+douglazarini@users.noreply.github.com>
2024-10-19 14:16:02 -0300

    Models 1

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),  # home
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),  # home
     path('recipes/<int:id>/', views.recipe, name="recipe"),  # home
 ]
